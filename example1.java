d2c1fd27d8784b8ba569ae04dc4f5da42a94572e
KulkarniDipali <88967673+KulkarniDipali@users.noreply.github.com>
2021-08-25 09:19:18 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,26 @@
+/* 11. Write a program in Java to print the result of the specified
+ operations.   
+Sample Output:
+Print the result of some specific operation :
+--------------------------------------------------
+Result of 1st expression is : 23
+Result of 2nd expression is : 5
+Result of 3rd expression is : 12
+Result of 4th expression is : 3  */
+
+
+public class example1 
+{
+ public static void main(String[] args)
+  {
+  System.out.print("Result of 1st expression is:");
+  System.out.println(-1+4*6);
+  System.out.print("Result of 2nd expression is:");
+  System.out.println((35+5)%7);
+  System.out.print("Result of 3rd expression is:");
+  System.out.println(14+-4*6/11);
+  System.out.print("Result of 3rd expression is:");
+  System.out.println(2+15/6*1-7%2);
+ }
+ 
+}
